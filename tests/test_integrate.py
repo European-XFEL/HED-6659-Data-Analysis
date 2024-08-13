daa81f1e67f2252503a5cf0638ffc366b433fe84
Clemens Prescher <clemens.prescher@gmail.com>
2024-08-13 13:46:07 +0200

    Implementation of integrate multiple

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -2,30 +2,97 @@ import os
 import h5py
 from . import unittest_data_path
 
-from xrd.integrate import integrate_single
+from xrd.integrate import (
+    integrate_single,
+    get_azimuthal_integrator,
+    integrate_multiple,
+    get_multi_geometry,
+)
 
-data_path = os.path.join(unittest_data_path, 'xrd')
+data_path = os.path.join(unittest_data_path, "xrd")
+
 
 def test_integrate_single(tmp_path):
-    projected_image_path = os.path.join(data_path, 'dummy.tif')
-    projected_poni_path = os.path.join(data_path, 'dummy.poni')
-    
-    integrate_single(projected_image_path, projected_poni_path, tmp_path / 'output.h5')
+    projected_image_path = os.path.join(data_path, "dummy.tif")
+    projected_poni_path = os.path.join(data_path, "dummy.poni")
 
-    assert (tmp_path / 'output.h5').exists()
+    integrate_single(projected_image_path, projected_poni_path, tmp_path / "output.h5")
 
-    with h5py.File(tmp_path / 'output.h5', 'r') as f:
-        assert 'pattern' in f
-        assert 'cake' in f
+    assert (tmp_path / "output.h5").exists()
 
-        assert 'x' in f['pattern']
-        assert 'y' in f['pattern']
-        assert 'intensity' in f['cake']
-        assert 'tth' in f['cake']
-        assert 'chi' in f['cake']
-        
-        assert f['pattern/x'].shape == (1000,)
-        assert f['pattern/y'].shape == (1000,)
-        assert f['cake/intensity'].shape == (360, 1000)
-        assert f['cake/tth'].shape == (1000,)
-        assert f['cake/chi'].shape == (360,)
+    with h5py.File(tmp_path / "output.h5", "r") as f:
+        assert "pattern" in f
+        assert "cake" in f
+
+        assert "x" in f["pattern"]
+        assert "y" in f["pattern"]
+        assert "intensity" in f["cake"]
+        assert "tth" in f["cake"]
+        assert "chi" in f["cake"]
+
+        assert f["pattern/x"].shape == (1000,)
+        assert f["pattern/y"].shape == (1000,)
+        assert f["cake/intensity"].shape == (360, 1000)
+        assert f["cake/tth"].shape == (1000,)
+        assert f["cake/chi"].shape == (360,)
+
+
+def test_ai_caching():
+    ai1 = get_azimuthal_integrator(os.path.join(data_path, "dummy.poni"), "1D", 1000)
+    ai2 = get_azimuthal_integrator(os.path.join(data_path, "dummy.poni"), "1D", 1000)
+    assert ai1 is ai2, "Should return the same object for the same poni_path and type"
+
+    ai2 = get_azimuthal_integrator(os.path.join(data_path, "dummy.poni"), "2D", 1000)
+    assert ai1 is not ai2, "Should return different objects for different types"
+
+    ai2 = get_azimuthal_integrator(os.path.join(data_path, "dummy.poni"), "1D", 2000)
+    assert ai1 is not ai2, "Should return different objects for different num_points"
+
+
+def test_integrate_multiple(tmp_path):
+    projected_image_path = os.path.join(data_path, "dummy.tif")
+    projected_image_paths = [projected_image_path, projected_image_path]
+    projected_poni_path = os.path.join(data_path, "dummy.poni")
+    projected_poni_paths = [projected_poni_path, projected_poni_path]
+
+    integrate_multiple(
+        projected_image_paths, projected_poni_paths, tmp_path / "output.h5", 1000
+    )
+
+    assert (tmp_path / "output.h5").exists()
+
+    with h5py.File(tmp_path / "output.h5", "r") as f:
+        assert "pattern" in f
+        assert "cake" in f
+
+        assert "x" in f["pattern"]
+        assert "y" in f["pattern"]
+        assert "intensity" in f["cake"]
+        assert "tth" in f["cake"]
+        assert "chi" in f["cake"]
+
+        assert f["pattern/x"].shape == (1000,)
+        assert f["pattern/y"].shape == (1000,)
+        assert f["cake/intensity"].shape == (360, 1000)
+        assert f["cake/tth"].shape == (1000,)
+        assert f["cake/chi"].shape == (360,)
+
+
+def test_mg_caching():
+    mg1 = get_multi_geometry(
+        [os.path.join(data_path, "dummy.poni")], "1D", 1000, radial_range=(0, 180)
+    )
+    mg2 = get_multi_geometry(
+        [os.path.join(data_path, "dummy.poni")], "1D", 1000, radial_range=(0, 360)
+    )
+    assert mg1 is not mg2, "Should return different objects for different radial ranges"
+
+    mg3 = get_multi_geometry([os.path.join(data_path, "dummy.poni")], "1D", 1000)
+    mg4 = get_multi_geometry([os.path.join(data_path, "dummy.poni")], "1D", 2000)
+    assert mg3 is not mg4, "Should return different objects for different num_points"
+
+    mg5 = get_multi_geometry([os.path.join(data_path, "dummy.poni")], "1D", 1000)
+    mg6 = get_multi_geometry([os.path.join(data_path, "dummy.poni")], "2D", 1000)
+    assert (
+        mg5 is not mg6
+    ), "Should return different objects for different integration types"
--- xrd/integrate.py
+++ xrd/integrate.py
@@ -1,29 +1,113 @@
-
 from pyFAI import AzimuthalIntegrator
+from pyFAI.multi_geometry import MultiGeometry
 import fabio
 import h5py
 
+ai_cache = {}
 
-def integrate_single(image_path, poni_path, output_path):
+
+def get_azimuthal_integrator(poni_path, integration_type, num_points):
+    """
+    Get an AzimuthalIntegrator object from the cache or create a new one if it doesn't exist. Use the cache to avoid
+    loading the same PONI file multiple times. (the initialization of the AzimuthalIntegrator object is expensive)
+
+    :param poni_path: path to the PONI file which contains the calibration parameters
+    :param integration_type: type of integration to perform (1D or 2D)
+    :param num_points: number of points to use for the integration
+    :return: AzimuthalIntegrator object
+    """
+    key = (poni_path, integration_type, num_points)
+    if key not in ai_cache:
+        ai_cache[key] = AzimuthalIntegrator()
+        ai_cache[key].load(poni_path)
+    return ai_cache[key]
+
+
+def integrate_single(image_path, poni_path, h5_output_path, num_points=1000):
     """
     Integrate a single image using a 1D and 2D integration and save the results to an HDF5 file.
 
     :param image_path: path to the image to integrate
     :param poni_path: path to the PONI file which contains the calibration parameters
-    :param output_path: path to the output HDF5 file
+    :param h5_output_path: path to the output HDF5 file
     """
-    ai_1d = AzimuthalIntegrator()
-    ai_1d.load(poni_path)
-
-    ai_2d = AzimuthalIntegrator()
-    ai_2d.load(poni_path)
+    ai_1d = get_azimuthal_integrator(poni_path, "1D", num_points)
+    ai_2d = get_azimuthal_integrator(poni_path, "2D", num_points)
 
     img = fabio.open(image_path).data
-    x, y = ai_1d.integrate1d(img, 1000, unit="2th_deg")
-    int, tth, chi = ai_2d.integrate2d(img, 1000, 360, unit="2th_deg")
+    x, y = ai_1d.integrate1d(img, num_points, unit="2th_deg")
+    int, tth, chi = ai_2d.integrate2d(img, num_points, 360, unit="2th_deg")
 
     # save xy and 2D integration results to HDF5 in individual groups (pattern, and cake)
-    with h5py.File(output_path, "w") as f:
+    with h5py.File(h5_output_path, "w") as f:
+        f.create_group("pattern")
+        f.create_dataset("pattern/x", data=x)
+        f.create_dataset("pattern/y", data=y)
+
+        f.create_group("cake")
+        f.create_dataset("cake/intensity", data=int)
+        f.create_dataset("cake/tth", data=tth)
+        f.create_dataset("cake/chi", data=chi)
+
+
+mg_cache = {}
+
+
+def get_multi_geometry(
+    poni_paths,
+    integration_type,
+    num_points,
+    radial_range=(0, 180),
+    azimuth_range=(-180, 180),
+):
+    """
+    Get a MultiGeometry object from the cache or create a new one if it doesn't exist. Use the cache to avoid
+    loading the same PONI file multiple times. (the initialization of the MultiGeometry object is expensive)
+
+    :param poni_paths: list of paths to the PONI files which contain the calibration parameters
+    :param integration_type: type of integration to perform ("1D" or "2D")
+    :param num_points: number of points in two theta to use for the integration
+    :param radial_range: radial range in two theta degrees for the 1D and 2d integration
+    :param azimuth_range: azimuthal range in degrees for the 2D integration
+    :return: MultiGeometry object
+    """
+    key = (tuple(poni_paths), integration_type, num_points, radial_range, azimuth_range)
+    if key not in mg_cache:
+        mg_cache[key] = MultiGeometry(
+            poni_paths,
+            unit="2th_deg",
+            radial_range=radial_range,
+            azimuth_range=azimuth_range,
+        )
+    return mg_cache[key]
+
+
+def integrate_multiple(
+    image_paths,
+    poni_paths,
+    h5_output_path,
+    num_points,
+    radial_range=(0, 90),
+    azimuth_range=(-180, 180),
+):
+    """
+    Integrate multiple images combining them into a pattern and Cake and save the results to an HDF5 file.
+
+    :param image_paths: list of paths to the images to integrate
+    :param poni_paths: list of paths to the PONI files which contain the calibration parameters
+    :param h5_output_path: path to the output HDF5 file
+    :param num_points: number of points in two theta to use for the integration
+    :param radial_range: radial range in two theta degrees for the 1D and 2d integration
+    :param azimuth_range: azimuthal range in degrees for the 2D integration
+    """
+    imgs = [fabio.open(image_path).data for image_path in image_paths]
+    mg_1d = get_multi_geometry(poni_paths, "1D", radial_range, azimuth_range)
+    mg_2d = get_multi_geometry(poni_paths, "2D", radial_range, azimuth_range)
+
+    x, y = mg_1d.integrate1d(imgs, num_points)
+    int, tth, chi = mg_2d.integrate2d(imgs, num_points, 360)
+
+    with h5py.File(h5_output_path, "w") as f:
         f.create_group("pattern")
         f.create_dataset("pattern/x", data=x)
         f.create_dataset("pattern/y", data=y)
