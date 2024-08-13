bd4a58a21148436f786c4fe925a4a7d168bc90ae
Clemens Prescher <clemens.prescher@gmail.com>
2024-08-13 10:51:46 +0200

    Naive implementation of integrate_single (no caching of the azimuthal integrator yet)

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -1,14 +1,31 @@
 import os
+import h5py
 from . import unittest_data_path
 
-from xrd.integrate import integrate_from_projected_image
+from xrd.integrate import integrate_single
 
 data_path = os.path.join(unittest_data_path, 'xrd')
 
-def test_integrate_from_projected_image():
+def test_integrate_single(tmp_path):
     projected_image_path = os.path.join(data_path, 'dummy.tif')
     projected_poni_path = os.path.join(data_path, 'dummy.poni')
     
-    x, y = integrate_from_projected_image(projected_image_path, projected_poni_path)
+    integrate_single(projected_image_path, projected_poni_path, tmp_path / 'output.h5')
 
-    print(x)
+    assert (tmp_path / 'output.h5').exists()
+
+    with h5py.File(tmp_path / 'output.h5', 'r') as f:
+        assert 'pattern' in f
+        assert 'cake' in f
+
+        assert 'x' in f['pattern']
+        assert 'y' in f['pattern']
+        assert 'intensity' in f['cake']
+        assert 'tth' in f['cake']
+        assert 'chi' in f['cake']
+        
+        assert f['pattern/x'].shape == (1000,)
+        assert f['pattern/y'].shape == (1000,)
+        assert f['cake/intensity'].shape == (360, 1000)
+        assert f['cake/tth'].shape == (1000,)
+        assert f['cake/chi'].shape == (360,)
--- xrd/integrate.py
+++ xrd/integrate.py
@@ -1,10 +1,34 @@
 
 from pyFAI import AzimuthalIntegrator
 import fabio
+import h5py
 
 
-def integrate_from_projected_image(projected_image_path, projected_poni_path):
-    ai = AzimuthalIntegrator()
-    ai.load(projected_poni_path)
-    img = fabio.open(projected_image_path).data
-    return ai.integrate1d(img, 1000, unit="2th_deg")
+def integrate_single(image_path, poni_path, output_path):
+    """
+    Integrate a single image using a 1D and 2D integration and save the results to an HDF5 file.
+
+    :param image_path: path to the image to integrate
+    :param poni_path: path to the PONI file which contains the calibration parameters
+    :param output_path: path to the output HDF5 file
+    """
+    ai_1d = AzimuthalIntegrator()
+    ai_1d.load(poni_path)
+
+    ai_2d = AzimuthalIntegrator()
+    ai_2d.load(poni_path)
+
+    img = fabio.open(image_path).data
+    x, y = ai_1d.integrate1d(img, 1000, unit="2th_deg")
+    int, tth, chi = ai_2d.integrate2d(img, 1000, 360, unit="2th_deg")
+
+    # save xy and 2D integration results to HDF5 in individual groups (pattern, and cake)
+    with h5py.File(output_path, "w") as f:
+        f.create_group("pattern")
+        f.create_dataset("pattern/x", data=x)
+        f.create_dataset("pattern/y", data=y)
+
+        f.create_group("cake")
+        f.create_dataset("cake/intensity", data=int)
+        f.create_dataset("cake/tth", data=tth)
+        f.create_dataset("cake/chi", data=chi)
